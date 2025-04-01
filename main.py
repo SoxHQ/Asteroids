e85c67e54a44ff87988568cf57759d2e8f3854bd
SoxHQ <soxnation98@gmail.com>
2025-03-31 22:24:58 -0400

    added some simple pygame logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import *
 
-
+i = 0
 
 def main():
     pygame.init()
@@ -9,6 +9,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    while i < 10:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
