a801bfb5b3d8881fa91873f6b7f5c81697f192db
SoxHQ <soxnation98@gmailcom>
2025-07-26 00:38:10 -0400

    finished!

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Asteroids
 
-BookBot is my second [Boot.dev](https://www.boot.dev) project!
+Asteroids is my second [Boot.dev](https://www.boot.dev) project!
--- main.py
+++ main.py
@@ -1,21 +1,60 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
-i = 0
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (updateable, drawable, shots)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    Player.containers = (updateable, drawable)
+    AsteroidField.containers = (updateable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
+    dt = 0
+
     
-    while i < 10:
+
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
+        updateable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+        
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
